72816994580476d69bebbc2222abc066b491ab30
daring <daring5920@outlook.com>
2023-05-29 21:03:21 +0800

    init

--- gmx-arbitrum-stats/src/helpers.ts
+++ gmx-arbitrum-stats/src/helpers.ts
@@ -9,12 +9,12 @@ export let PRECISION = BigInt.fromI32(10).pow(30)
 
 // must be lower
 export let WETH = "0x4200000000000000000000000000000000000006"
-export let BTC = "0x5fe699663197083711137060e3774b9ecdee16d2"
-export let LINK = "0x1ef0a4cd5cd4cd23d3dffb8ec657d92206b11005"
-export let USDT = "0xdcd63e6142a900dc1e1780595b2d165c6459104e"
-export let USDC = "0x4f526361313618a87e952720f3ef5b44dcd7c9c3"
-export let DAI = "0xaa23a4d90a50a06e171d32ee75897cdca4bc3f51"
-export let EDDX = "0x9fd329310b43d00aaf050518200732ac45f34dc5"
+export let BTC = "0x1acf131de5bbc72ae96ee5ec7b59da2f38b19dbd"
+export let LINK = "0x63ba205da17003ab46ce0dd78be8ba8ee3952e5f"
+export let USDT = "0x8654f060eb1e5533c259cdcbbe39834bb8141cf4"
+export let USDC = "0xecb03bbcf83e863b9053a926932dbb07d837ebbe"
+export let DAI = "0xfe9cdcc77fb826b380d49f53c8ce298b600cb7f0"
+export let EDDX = "0x24b63ae170152fccf6a11cd77ffa2d7f04ed999d"
 
 export function timestampToDay(timestamp: BigInt): BigInt {
   return timestamp / BigInt.fromI32(86400) * BigInt.fromI32(86400)
